1b77c46dcceb841f1435c9643e400be19fea48d1
Bogdan Kostadinov <bogdan.kostadinov1@gmail.com>
2024-01-30 19:19:08 +0200

    reorganizing file structure and adding a projects=page-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMePageComponent } from './about-me-page/about-me-page.component';
 import { AppComponent } from './app.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
 const routes: Routes = [
   {
     path: 'about-me',
     component: AboutMePageComponent,
+  },
+  {
+    path: 'projects-page',
+    component: ProjectsPageComponent,
   },
   { path: '**', component: AppComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { AboutMePageComponent } from './about-me-page/about-me-page.component';
+import { BibliographySectionComponent } from './about-me-page/bibliography-section-component/bibliography-section-component.component';
+import { SkillsSectionComponent } from './about-me-page/skills-section/skills-section.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BibliographySectionComponent } from './bibliography-section-component/bibliography-section-component.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 import { CardComponent } from './shared/components/card-component/card-component';
 import { ConfirmWindowComponent } from './shared/components/confirm-window/confirm-window.component';
-import { MaterialModule } from './shared/material.module';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { SkillsSectionComponent } from './skills-section/skills-section.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { MaterialModule } from './shared/modules/material.module';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { SkillsSectionComponent } from './skills-section/skills-section.componen
     AboutMePageComponent,
     CardComponent,
     ConfirmWindowComponent,
+    ProjectsPageComponent
   ],
   imports: [
     BrowserModule,
